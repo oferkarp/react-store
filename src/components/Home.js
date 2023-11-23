5cb00313bb8197840d7d5dcb4bf1989c6d7c4f35
ofer <oferc008@gmail.com>
2023-11-23 19:08:38 +0200

    fix bugs

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="text-center">
+        <h1>Welcome to my Store</h1>
+        <p className="mb-4">Explore our collection of products.</p>
+        <Link to="/" className="btn btn-primary">
+          View Products
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
