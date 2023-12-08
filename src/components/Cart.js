bc7981688d8ba1a7b469edbb6878f260c515510a
ofer <oferc008@gmail.com>
2023-12-08 22:07:18 +0200

    add clear cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,93 +1,87 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import '../App.css';
+import '../App.css'
 
 function Cart() {
-  const [cartItems, setCartItems] = useState([]);
-  const [productData, setProductData] = useState({});
-  const { userId } = useParams();
+  const [cartItems, setCartItems] = useState([])
+  const [productData, setProductData] = useState({})
+  const { userId } = useParams()
 
   useEffect(() => {
     const fetchCartItems = async () => {
       try {
-        const response = await fetch(`https://django-rest-framework-store.onrender.com/user_cart_items/${userId}`);
+        const response = await fetch(`https://django-rest-framework-store.onrender.com/user_cart_items/${userId}`)
         if (response.ok) {
-          const responseData = await response.json();
-          const consolidatedCart = [];
-  
+          const responseData = await response.json()
+          const consolidatedCart = []
+
           responseData.forEach((cartItem) => {
-            const existingProduct = consolidatedCart.find(
-              (item) => item.product === cartItem.product
-            );
-  
+            const existingProduct = consolidatedCart.find((item) => item.product === cartItem.product)
+
             if (existingProduct) {
-              existingProduct.quantity += cartItem.quantity;
+              existingProduct.quantity += cartItem.quantity
             } else {
-              consolidatedCart.push(cartItem);
+              consolidatedCart.push(cartItem)
             }
-          });
-  
-          setCartItems(consolidatedCart);
+          })
+
+          setCartItems(consolidatedCart)
         } else {
-          console.error('Failed to fetch cart items');
+          console.error('Failed to fetch cart items')
         }
       } catch (error) {
-        console.error('Error fetching cart items:', error);
+        console.error('Error fetching cart items:', error)
       }
-    };
-  
-    fetchCartItems();
-  }, [userId]);
+    }
 
+    fetchCartItems()
+  }, [userId])
 
   useEffect(() => {
     const fetchProduct = async (productId) => {
       try {
-        const response = await fetch(`https://django-rest-framework-store.onrender.com/products/${productId}`);
+        const response = await fetch(`https://django-rest-framework-store.onrender.com/products/${productId}`)
         if (response.ok) {
-          const product = await response.json();
-          setProductData((prevData) => ({ ...prevData, [productId]: product }));
+          const product = await response.json()
+          setProductData((prevData) => ({ ...prevData, [productId]: product }))
         } else {
-          console.error(`Failed to fetch product details for product ID: ${productId}`);
+          console.error(`Failed to fetch product details for product ID: ${productId}`)
         }
       } catch (error) {
-        console.error('Error fetching product:', error);
+        console.error('Error fetching product:', error)
       }
-    };
+    }
 
     cartItems.forEach((cartItem) => {
       if (!productData[cartItem.product]) {
-        fetchProduct(cartItem.product);
+        fetchProduct(cartItem.product)
       }
-    });
-  }, [cartItems, productData]);
+    })
+  }, [cartItems, productData])
 
   const deleteCartItem = async (productId) => {
     try {
-      const response = await axios.delete(`https://django-rest-framework-store.onrender.com/delete_cart_item/${userId}/${productId}/`);
+      const response = await axios.delete(`https://django-rest-framework-store.onrender.com/delete_cart_item/${userId}/${productId}/`)
       if (response.status === 204) {
         // Cart item deleted successfully
         // console.log('Cart item deleted');
         // Update the cart view if needed
-        const updatedCartItems = cartItems.filter((item) => item.product !== productId);
-        setCartItems(updatedCartItems);
+        const updatedCartItems = cartItems.filter((item) => item.product !== productId)
+        setCartItems(updatedCartItems)
       } else {
-        console.error('Failed to delete cart item');
+        console.error('Failed to delete cart item')
       }
     } catch (error) {
-      console.error('Error deleting cart item:', error);
+      console.error('Error deleting cart item:', error)
     }
-  };
+  }
 
   const checkout = async () => {
     try {
-      const csrftoken = localStorage.getItem('token');
-  
-      console.log('CSRF Token:', csrftoken);
-      console.log('Cart Items:', cartItems);
-  
-      const authToken = localStorage.getItem('token'); // Retrieve the authentication token from local storage
+      console.log('Cart Items:', cartItems)
+
+      const authToken = localStorage.getItem('token') // Retrieve the authentication token from local storage
 
       const response = await axios.post(
         `https://django-rest-framework-store.onrender.com/checkout/`,
@@ -100,51 +94,87 @@ function Cart() {
             'Content-Type': 'application/json',
           },
         }
-      );
-  
+      )
+
       if (response.status === 201) {
         // Order created successfully
-        setCartItems([]); // Clear the cart items after creating the order
+        setCartItems([]) // Clear the cart items after creating the order
+        clearCart(userId)
       } else {
-        console.error('Failed to create order');
+        console.error('Failed to create order')
       }
     } catch (error) {
-      console.error('Error creating order:', error);
+      console.error('Error creating order:', error)
     }
-  };
-  
+  }
+
+  const clearCart = async (userId) => {
+    try {
+      const authToken = localStorage.getItem('token')
+
+      const response = await axios.post(
+        `https://django-rest-framework-store.onrender.com/clear_cart/${userId}/`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+
+      if (response.status === 200) {
+        console.log('Cart cleared successfully')
+        // Perform any additional actions after clearing the cart
+      } else {
+        console.error('Failed to clear cart')
+      }
+    } catch (error) {
+      console.error('Error clearing cart:', error)
+    }
+  }
+
   return (
-    <div class="cart-container">
-    <h2>Cart</h2>
-    {cartItems.length > 0 ? (
-      <div class="cart-items">
-        {cartItems.map((cartItem) => {
-          const product = productData[cartItem.product];
-          return (
-            <div class="cart-item" key={cartItem.id}>
-              {product && (
-                <>
-                  <img src={`https://django-rest-framework-store.onrender.com${product.image}`} alt={product.name} class="product-image" />
-                  <div class="item-details">
-                    <p><strong>Name:</strong> {product.name}</p>
-                    <p><strong>Price:</strong> ${parseFloat(product.price).toFixed(2)}</p>
-                    <p><strong>Quantity:</strong> {cartItem.quantity}</p>
-                    <button onClick={() => deleteCartItem(cartItem.product)}>Remove</button>
-                  </div>
-                </>
-              )}
-            </div>
-          );
-        })}
-      </div>
-    ) : (
-      <p>Your cart is empty.</p>
-    )}
-    {cartItems.length > 0 && (
-        <button onClick={checkout}>Checkout</button>
+    <div className="cart-container">
+      <h2>Cart</h2>
+      {cartItems.length > 0 ? (
+        <div className="cart-items">
+          {cartItems.map((cartItem) => {
+            const product = productData[cartItem.product]
+            return (
+              <div className="cart-item" key={cartItem.id}>
+                {product && (
+                  <>
+                    <img src={`https://django-rest-framework-store.onrender.com${product.image}`} alt={product.name} className="product-image" />
+                    <div className="item-details">
+                      <p>
+                        <strong>Name:</strong> {product.name}
+                      </p>
+                      <p>
+                        <strong>Price:</strong> ${parseFloat(product.price).toFixed(2)}
+                      </p>
+                      <p>
+                        <strong>Quantity:</strong> {cartItem.quantity}
+                      </p>
+                      <button onClick={() => deleteCartItem(cartItem.product)}>Remove</button>
+                    </div>
+                  </>
+                )}
+              </div>
+            )
+          })}
+        </div>
+      ) : (
+        <p>Your cart is empty.</p>
       )}
-  </div>
-  );
+      {cartItems.length > 0 && (
+        <div>
+          <button onClick={checkout}>Checkout</button>
+          <button onClick={() => clearCart(userId)}>Clear Cart</button>
+        </div>
+      )}
+    </div>
+  )
 }
 
-export default Cart;
+export default Cart
