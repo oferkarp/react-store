34964da1365fd5592aac54c5eaac0c70d985b47c
ofer <oferc008@gmail.com>
2023-12-11 01:14:59 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import Product from './components/product'
 import Navbar from './components/Navbar'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Login from './components/Login'
 import NoPage from './components/NoPage'
 import MyFooter from './components/MyFooter'
@@ -11,7 +11,7 @@ import Home from './components/Home'
 import Cart from './components/Cart'
 import Registration from './components/Registration'
 import Order from './components/Order'
-import { HashRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 
 function App() {
